a43a409ddfde45ccae5ce41552890c971d749742
evilkau <146204994+evilkau@users.noreply.github.com>
2025-02-26 10:46:49 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,11 @@
+const valor1 = 12333636352;
+const valor2 = 928262622;
+let soma = valor1 + valor2;
+let sub = valor1 - valor2;
+let mult = valor1 * valor2;
+let div = valor1 / valor2;
+
+console.log(" A soma e:" + soma);
+console.log("A subtracao e:" + sub);
+console.log("A multiplicao e:" + mult)
+console.log("A divisao e:"+ div)
---
+++ aula1.py
@@ -0,0 +1,23 @@
+import random 
+
+sorteio = random.randint(1,10)
+chute = input("digite o seu chute:")
+print(sorteio)
+print (chute)
+ chute=int (input("chute"))
+ limite_tentativas= 3
+ tentativas = 1
+ while (limite_tentativas >= tentativas)
+    print("tentativas:",tentativas)
+    chute= int(input("digite o seu chute"))
+    if (chute==sorteio):
+    print(Parabens,voce acertou)
+    #tentativas = tentativas +1
+    #print (sorteio)
+    #print(chute)
+    print(sorteio)
+    print (chute)
+    if (chute==sorteio):
+    print("Parabens,voce acertou qual numero era",sorteio)
+    elif(chute>sorteio):
+        print(chute um s)
