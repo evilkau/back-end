65d2003167626803d25c13d626027d8f239521be
evilkau <146204994+evilkau@users.noreply.github.com>
2025-04-14 12:06:32 -0300

    Add files via upload

--- livro.js
+++ livro.js
@@ -7,6 +7,24 @@ function mostraLivros() {
     });
 }
 
+function adicionarlivro(titulo,autor,editora,ano,sinopse,paginas,genero,preco){
+    livros.push({
+        titulo:titulo,
+        autor:autor,
+        editora:editora,
+        ano:ano,
+        sinopse:sinopse,
+        paginas:paginas,
+        genero:genero,
+        preco:preco,
+    });
+    console.log("livro adicionado com sucesso");
+
+}
+adicionarlivro("O Pequeno Principe","Leonardo Devinci","Tilibra",2025,
+"Um principe que era pequno",1,["terror","webtoon","comedia"],500.99);
+
+
 //Gera um arquivo JSON dos livros
 function criarArquivo(){
     let livroTexto = JSON.stringify(livros);
